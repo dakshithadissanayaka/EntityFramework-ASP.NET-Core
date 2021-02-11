3b773c5fff5ad25008109f2abd3a5164b892856c
Dakshitha Dissanayaka <45692186+Dakshitha96@users.noreply.github.com>
2021-02-11 22:57:15 +0530

    Source code

---
+++ BookStoresWebAPI/BookStoresWebAPI/Migrations/20210211014111_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStoresWebAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Publishers",
+                columns: table => new
+                {
+                    PublisherID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PublisherName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publishers", x => x.PublisherID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Publishers");
+        }
+    }
+}
